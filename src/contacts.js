529698e743f8dcd1eed7066667f60ed700e2e426
Khoa Hoang <khoahoang1698@gmail.com>
2024-03-08 11:18:06 -0800

    update

--- src/contacts.js
+++ src/contacts.js
@@ -3,7 +3,7 @@ const contacts = [
       id: 1,
       name: "Beyonce",
       imgURL:
-        "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+        "https://www.naturewise.com/cdn/shop/files/NW---Elderberry-60-Count-PDP-Image.jpg",
       phone: "+123 456 789",
       email: "b@beyonce.com"
     },
@@ -11,7 +11,7 @@ const contacts = [
       id: 2,
       name: "Jack Bauer",
       imgURL:
-        "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+        "https://www.naturewise.com/cdn/shop/files/NW-VitaminCChewablePDPImage.jpg",
       phone: "+987 654 321",
       email: "jack@nowhere.com"
     },
@@ -19,7 +19,7 @@ const contacts = [
       id: 3,
       name: "Chuck Norris",
       imgURL:
-        "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+        "https://www.naturewise.com/cdn/shop/files/1b-GummyAshwagandha-Bottle.jpg",
       phone: "+918 372 574",
       email: "gmail@chucknorris.com"
     }
